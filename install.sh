87ddef8be16f1a08b958fd4793e2996cc7d115e7
kharl <kharl.dms112@gmail.com>
2024-03-07 14:09:12 +0000

    use plug instead of relying to directories

--- install.sh
+++ install.sh
@@ -2,11 +2,13 @@
 
 #install nvim
 sudo apt-get update
-sudo apt-get install neovim git -y
+sudo apt-get install neovim git curl -y
+
+set -ex
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#install conf
+cat vimrc.conf > ~/.vimrc
 
 
-#Officially, Vim recommends that each plugin project gets its own directory within ~/.vim/pack. 
-mkdir -p ~/.vim/pack/vendor/start
 
-#install nerdtree plugin into the directory
-git clone --depth 1 https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
