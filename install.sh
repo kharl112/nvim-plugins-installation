4178a5a984f69b60261ffddbcc24d8bd653e3708
kharl.dms112@gmail.com <kharl.dms112@gmail.com>
2024-03-09 01:54:39 +0000

    conf used nvim. mistakenly used vim

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ sudo apt-get update
 sudo apt-get install neovim git curl -y
 
 set -ex
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #install conf
-cat vimrc.conf > ~/.vimrc
+mkdir -p ~/.config/nvim
+touch ~/.config/nvim/init.vim
+cat vimrc.conf > ~/.config/nvim/init.vim
 
 
 
