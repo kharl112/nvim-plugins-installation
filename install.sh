b0c445ec7a9b538139ecf304dc36fd8489f0efb0
kharl112 <kharl.dms112@gmail.com>
2026-02-24 09:27:50 +0000

    added codecompanion

--- install.sh
+++ install.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
 #install nvim
-sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen
-cd /tmp
-git clone https://github.com/neovim/neovim.git
-cd neovim
-make CMAKE_BUILD_TYPE=Release
-sudo make install
+sudo apt install ninja-build gettext libtool libtool-bin autoconf automake cmake g++ pkg-config unzip curl doxygen zstd
+if ! command -v nvim --version >/dev/null 2>&1; then
+  current_dir=$PWD
+  cd /tmp
+  git clone https://github.com/neovim/neovim.git
+  cd neovim
+  make CMAKE_BUILD_TYPE=Release
+  sudo make install
+  nvim --version
+  cd $current_dir 
+fi
 
-cd -
 
 sudo apt-get update
 sudo apt-get install tmux -y
+sudo apt autoremove -y
+
+
+# Check if Ollama is installed
+if ! command -v ollama >/dev/null 2>&1; then
+  read -p "Ollama not found. Install it? (y/n): " confirm
+  if [ "$confirm" != "y" ]; then
+     curl -fsSL https://ollama.com/install.sh | sh
+     reset 
+     deepcoder:1.5b
+  fi
+fi
 
 set -ex
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
